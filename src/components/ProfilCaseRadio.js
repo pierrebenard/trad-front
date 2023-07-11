1905aceb1a4ebb04fad952357fb0d3e866bb33d6
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-07-11 08:48:16 +0100

    me

--- src/components/ProfilCaseRadio.js
+++ src/components/ProfilCaseRadio.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from 'moment';
+import { format } from "date-fns";
 
 import CaseRadio from './CaseRadio';
 import Button from './Button';
@@ -11,102 +15,130 @@ const ProfilCaseRadio = ({ selectedOption }) => {
   const [rrRange, setRrRange] = useState(null);
   const [otherSelectedOption, setOtherSelectedOption] = useState('');
   const [secondSelectedOption, setSecondSelectedOption] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
   const options = [
-    { value: "aujourd'hui", label: "Aujourd'hui" },
-    { value: 'semaineEnCours', label: 'Semaine en cours' },
-    { value: 'semaineGlissante', label: 'Semaine glissante' },
-    { value: 'moisGlissant', label: 'Mois glissant' },
-    { value: 'moisEnCours', label: 'Mois en cours' },
-    { value: '3mois', label: '3 mois' },
-    { value: '6mois', label: '6 mois' },
-    { value: '1an', label: '1 an' },
-    { value: '2ans', label: '2 ans' },
-    { value: 'tout', label: 'Tout' },
-    { value: 'choixlibre', label: 'Choix libre' },
+    { value: "aujourd'hui", label: "Aujourd'hui" }, { value: 'semaineEnCours', label: 'Semaine en cours' }, { value: 'semaineGlissante', label: 'Semaine glissante' },
+    { value: 'moisGlissant', label: 'Mois glissant' }, { value: 'moisEnCours', label: 'Mois en cours' }, { value: '3mois', label: '3 mois' }, { value: '6mois', label: '6 mois' }, 
+    { value: '1an', label: '1 an' }, { value: '2ans', label: '2 ans' }, { value: 'tout', label: 'Tout' }, { value: 'choixLibre', label: 'Choix libre' },
   ];
   const secondOptions = [
-    { value: "DowJonesIndustrialAverage", label: "Dow Jones Industrial Average" },
-    { value: 'NasdaqComposite', label: 'Nasdaq Composite' },
-    { value: 'FTSE100', label: 'FTSE 100' },
-    { value: 'DAX30', label: 'DAX 30' },
-    { value: 'CAC40', label: 'CAC 40' },
-    { value: 'Nikkei225', label: 'Nikkei 225' },
-    { value: 'ShangaiComposite', label: 'Shangai Composite' },
-    { value: 'HangSengIndex', label: 'Hang Seng Index' },
-    { value: 'S&P/ASX200', label: 'S&P/ASX 200' },
-    { value: 'Bovespa', label: 'Bovespa' },
-    { value: 'Nifty50', label: 'Nifty 50' },
-    { value: 'TAIEX', label: 'TAIEX' },
-    { value: 'RTSIndex', label: 'RTS Index' },
-    { value: 'S&P/TSXComposite', label: 'S&P/TSX Composite' },
+    { value: "DowJonesIndustrialAverage", label: "Dow Jones Industrial Average" }, { value: 'NasdaqComposite', label: 'Nasdaq Composite' }, { value: 'FTSE100', label: 'FTSE 100' },
+    { value: 'DAX30', label: 'DAX 30' }, { value: 'CAC40', label: 'CAC 40' }, { value: 'Nikkei225', label: 'Nikkei 225' },
+    { value: 'ShangaiComposite', label: 'Shangai Composite' }, { value: 'HangSengIndex', label: 'Hang Seng Index' }, { value: 'S&P/ASX200', label: 'S&P/ASX 200' },
+    { value: 'Bovespa', label: 'Bovespa' }, { value: 'Nifty50', label: 'Nifty 50' }, { value: 'TAIEX', label: 'TAIEX' },
+    { value: 'RTSIndex', label: 'RTS Index' }, { value: 'S&P/TSXComposite', label: 'S&P/TSX Composite' }, { value: 'ADAUSD', label: 'ADAUSD' },
   ];
 
   const valeurTakeProfitChange = (value) => {
     setTakeProfit(value);
   };
-
   const valeurStopLossChange = (value) => {
     setStopLoss(value);
   };
-
   const valeurRrRangeChange = (value) => {
     setRrRange(value);
   };
 
-  const soumissionsFormulaire = async () => {
+  const handleStartDateChange = (date) => {
+    if (date) {
+      setStartDate(date);
+      console.log("Date de début :", date);
+    }
+  };
+  
+  const handleEndDateChange = (date) => {
+    if (date) {
+      setEndDate(date);
+      console.log("Date de fin :", date);
+    }
+  };
+  
 
+  const soumissionsFormulaire = async () => {
     let argumentDate = "";
     if (otherSelectedOption === "aujourd'hui") {
       argumentDate = "aujourd'hui";
-      console.log(argumentDate)
     } else if (otherSelectedOption === "semaineEnCours") {
       argumentDate = "semaineEnCours";
-      console.log(argumentDate)
     } else if (otherSelectedOption === "semaineGlissante") {
       argumentDate = "semaineGlissante";
-      console.log(argumentDate)
     } else if (otherSelectedOption === "moisGlissant") {
       argumentDate = "moisGlissant";
-      console.log(argumentDate)
     } else if (otherSelectedOption === "moisEnCours") {
       argumentDate = "moisEnCours";
-      console.log(argumentDate)
+    } else if (otherSelectedOption === "choixLibre") {
+      argumentDate = "choixLibre";
     }
 
-    // Deuxième sélecteur
     let argumentFiltre = "";
-    if (secondSelectedOption === "option1") {
-      argumentFiltre = "valeur1";
-      console.log(argumentFiltre)
-    } else if (secondSelectedOption === "option2") {
-      argumentFiltre = "valeur2";
-      console.log(argumentFiltre)
-    } else if (secondSelectedOption === "option3") {
-      argumentFiltre = "valeur3";
-      console.log(argumentFiltre)
+    if (secondSelectedOption === "DowJonesIndustrialAverage") {
+      argumentFiltre = "Dow Jones Industrial Average";
+    } else if (secondSelectedOption === "NasdaqComposite") {
+      argumentFiltre = "Nasdaq Composite";
+    } else if (secondSelectedOption === "FTSE100") {
+      argumentFiltre = "FTSE 100";
+    } else if (secondSelectedOption === "DAX30") {
+      argumentFiltre = "DAX 30";
+    } else if (secondSelectedOption === "CAC40") {
+      argumentFiltre = "CAC 40";
+    } else if (secondSelectedOption === "Nikkei225") {
+      argumentFiltre = "Nikkei 225";
+    } else if (secondSelectedOption === "ShangaiComposite") {
+      argumentFiltre = "Shangai Composite";
+    } else if (secondSelectedOption === "HangSengIndex") {
+      argumentFiltre = "Hang Seng Index";
+    } else if (secondSelectedOption === "S&P/ASX200") {
+      argumentFiltre = "S&P/ASX 200";
+    } else if (secondSelectedOption === "Bovespa") {
+      argumentFiltre = "Bovespa";
+    } else if (secondSelectedOption === "Nifty50") {
+      argumentFiltre = "Nifty 50";
+    } else if (secondSelectedOption === "TAIEX") {
+      argumentFiltre = "TAIEX";
+    } else if (secondSelectedOption === "RTSIndex") {
+      argumentFiltre = "RTS Index";
+    } else if (secondSelectedOption === "S&P/TSXComposite") {
+      argumentFiltre = "S&P/TSX Composite";
+    } else if (secondSelectedOption === "ADAUSD") {
+      argumentFiltre = "ADAUSD";
     }
 
-    // appel à l'api
     if (takeProfit === 'Atteint') {
       const takeProfitRequeteAtteint = async () => {
         try {
-          const response = await axios.get(`http://127.0.0.1:5000/TPRatteint?date=${argumentDate}&filtre=${argumentFiltre}`);
-          console.log(response.data);
-          return response.data;
-        } catch (error) {console.error(error); throw error;}
+          let response;
+          if (startDate && endDate) {
+            const formattedStartDate = new Date(startDate).toISOString();
+            const formattedEndDate = new Date(endDate).toISOString();
+            response = await axios.get(`http://127.0.0.1:5000/TPRatteint?date=${argumentDate}&debutDate=${formattedStartDate}&finDate=${formattedEndDate}&filtre=${argumentFiltre}`);
+            console.log(response.data); return response.data;
+          } else {
+            response = await axios.get(`http://127.0.0.1:5000/TPRatteint?date=${argumentDate}&filtre=${argumentFiltre}`);
+            console.log(response.data); return response.data;
+          }
+        } catch (error) { console.error(error); throw error;}
       };
+      
       await takeProfitRequeteAtteint();
     } else if (takeProfit === 'Dépassé') {
     } else if (takeProfit === 'Non atteint') {
-      const takeProfitRequeteAtteint = async () => {
+      const takeProfitRequeteNonAtteint = async () => {
         try {
-          const response = await axios.get(`http://127.0.0.1:5000/TPRnonAtteint?date=${argumentDate}&filtre=${argumentFiltre}`);
-          console.log(response.data);
-          return response.data;
-        } catch (error) {console.error(error); throw error;}
+          let response;
+          if (startDate && endDate) {
+            const formattedStartDate = new Date(startDate).toISOString();
+            const formattedEndDate = new Date(endDate).toISOString();
+            response = await axios.get(`http://127.0.0.1:5000/TPRnonAtteint?date=${argumentDate}&debutDate=${formattedStartDate}&finDate=${formattedEndDate}&filtre=${argumentFiltre}`);
+            console.log(response.data); return response.data;
+          } else {
+            response = await axios.get(`http://127.0.0.1:5000/TPRnonAtteint?date=${argumentDate}&filtre=${argumentFiltre}`);
+            console.log(response.data); return response.data;
+          }
+        } catch (error) { console.error(error); throw error;}
       };
-      await takeProfitRequeteAtteint();
+      await takeProfitRequeteNonAtteint();
     }
 
     if (stopLoss === 'Atteint') {
@@ -122,20 +154,14 @@ const ProfilCaseRadio = ({ selectedOption }) => {
 
   return (
     <div>
-      {/* Premier sélecteur */}
-      <Selector
-        options={options}
-        value={otherSelectedOption}
-        onChange={(selected) => setOtherSelectedOption(selected)}
-      />
-
-      {/* Deuxième sélecteur */}
-      <Selector
-        options={secondOptions}
-        value={secondSelectedOption}
-        onChange={(selected) => setSecondSelectedOption(selected)}
-      />
-      
+      <Selector options={options} value={otherSelectedOption} onChange={(selected) => setOtherSelectedOption(selected)}/>
+      {otherSelectedOption === "choixLibre" && (
+        <div>
+          <DatePicker selected={startDate} onChange={handleStartDateChange} />
+          <DatePicker selected={endDate} onChange={handleEndDateChange} />
+        </div>
+      )}
+      <Selector options={secondOptions} value={secondSelectedOption} onChange={(selected) => setSecondSelectedOption(selected)}/>
       <CaseRadio titre="Take profit" nomOption1="Atteint" nomOption2="Dépassé" nomOption3="Non atteint" selectedCaseOption={takeProfit} onChange={valeurTakeProfitChange}/>
       <CaseRadio titre="Stop loss" nomOption1="Atteint" nomOption2="Partiel" selectedCaseOption={stopLoss} onChange={valeurStopLossChange}/>
       <CaseRadio titre="-0.5RR < BE < 0.5RR" nomOption1=" " selectedCaseOption={rrRange} onChange={valeurRrRangeChange}/>
